f4fa15c535469af6b2ffd4c37707062b4d5f6181
Boris Klco <klco@pm.me>
2023-05-16 12:13:33 +0000

    Od srdiecka nenavidim cele css a frontend

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -4,18 +4,32 @@ import { HiOutlineHome } from "react-icons/hi";
 
 const SideBar = () => {
   return (
-    <div
-      className="fixed top-0 left-0 h-screen lg:w-1/5 md:w-1/4 w-1/3 px-4 flex flex-col 
+    <>
+      <div className="flex">
+        <div
+          className="fixed top-0 left-0 h-screen lg:w-1/5 md:w-1/4 w-1/3 px-4 flex flex-col 
     bg-gray-900 text-white shadow-lg
     border-r-2 border-solid border-slate-600"
-    >
-      <h2 className="mb-8 mt-8 pl-8 text-2xl text-left">InstaClone</h2>
+        >
+          <h2 className="mb-8 mt-8 pl-8 text-2xl text-left">InstaClone</h2>
 
-      <SideBarIcon icon={<HiOutlineHome size="32px" />} text="Something" />
-      <SideBarIcon icon={<BiSearchAlt size="32px" />} text="Something" />
-      <SideBarIcon icon={<FiSend size="32px" />} text="Something" />
-      <SideBarIcon icon={<BiHeart size="32px" />} text="Something" />
-    </div>
+          <SideBarIcon icon={<HiOutlineHome size="32px" />} text="Something" />
+          <SideBarIcon icon={<BiSearchAlt size="32px" />} text="Something" />
+          <SideBarIcon icon={<FiSend size="32px" />} text="Something" />
+          <SideBarIcon icon={<BiHeart size="32px" />} text="Something" />
+        </div>
+        <div className="w-2/3 lg:w-4/5">
+          <p>Something</p>
+          <p>Something</p>
+          <p>Something</p>
+          <p>Something</p>
+          <p>Something</p>
+          <p>Something</p>
+          <p>Something</p>
+          <p>Something</p>
+        </div>
+      </div>
+    </>
   );
 };
 
